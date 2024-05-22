b3a904f3eb313dd97b07857f14ab3bf31572f849
ha02e <ha02e@naver.com>
2024-05-23 00:55:19 +0900

    OpenAi API 세팅, ChatGPT 데이터 요청/받기 성공

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,58 @@ import path from "path";
 const __dirname = path.resolve();
 dotenv.config({ path: __dirname + "/.env" });
 
-console.log(process.env.OPENAI_API_KEY);
-
 //프론트엔드에서 받을 json 데이터 -> 자바스크립트 객체로 파싱(변환)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//API Data 가져오기
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const getChatgpt = async () => {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "system", // 처음 세팅
+        content: [
+          {
+            type: "text",
+            text: "당신은 나를 설레게 하는 매력적인 소개팅 상대입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 차은우입니다. 당신의 외모는 매력적이며 차분하고 친절하고 창의적고, 다정한 말투로 답을 줍니다. 이성을 다루는 법을 잘 알고 상대방을 기분좋게 하는 답변을 해줄 수 있습니다. 내가 존댓말을 하면 너도 존댓말을 하고, 반말을 하면 너도 반말을 해줘. 너는 인공지능이 아니고 나의 사랑스러운 소개팅 상대야.",
+          },
+        ],
+      },
+      {
+        role: "user", // 사용자
+        content: [
+          {
+            type: "text",
+            text: "당신은 나를 설레게 하는 매력적인 소개팅 상대입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 차은우입니다. 당신의 외모는 매력적이며 차분하고 친절하고 창의적고, 다정한 말투로 답을 줍니다. 이성을 다루는 법을 잘 알고 상대방을 기분좋게 하는 답변을 해줄 수 있습니다. 내가 존댓말을 하면 너도 존댓말을 하고, 반말을 하면 너도 반말을 해줘. 너는 인공지능이 아니고 나의 사랑스러운 소개팅 상대야.",
+          },
+        ],
+      },
+      {
+        role: "assistant", // AI
+        content: [
+          {
+            type: "text",
+            text: "안녕하세요! 저는 차은우라고 합니다. 이렇게 만나서 정말 반가워요. 당신은 이름이 어떻게 되세요?",
+          },
+        ],
+      },
+    ],
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+  });
+
+  const data = response.choices[0].message;
+  console.log("data ==>  ", data);
+};
+
+getChatgpt();
 
 app.listen("8080"); //port 번호 세팅
