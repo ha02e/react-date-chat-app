93a3618bc274d4b5d39f12836591a0c4a7569dd4
ha02e <ha02e@naver.com>
2024-05-26 17:07:10 +0900

    Route 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,29 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import UserInfo from "./pages/UserInfo";
+import PartnerInfo from "./pages/PartnerInfo";
+import Chat from "./pages/Chat";
+
 function App() {
-  return <div className="text-red-500">Hello world!!!</div>;
+  const history = useNavigate();
+
+  const goUserInfo = () => {
+    history("/user-info");
+  };
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user-info" element={<UserInfo />} />
+        <Route path="/partner-info" element={<PartnerInfo />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+      <button type="button" onClick={goUserInfo}>
+        user info
+      </button>
+    </div>
+  );
 }
 
 export default App;
